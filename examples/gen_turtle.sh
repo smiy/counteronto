8c079de07bbadaf7b35b004fdc27873897f767f4
Bo Ferri <zazi@smiy.org>
2010-07-15 12:45:45 +0000

    counteronto: * MODIFIED: webcounter_-_example.* to webpagecounter_-_example.*

--- examples/gen_turtle.sh
+++ examples/gen_turtle.sh
@@ -2,5 +2,5 @@
 
 # Generate the Turtle from the RDF/XML code
 
-rapper -i rdfxml -o turtle RDF/webcounter_-_example.rdf > N3/webcounter_-_example.n3
+rapper -i rdfxml -o turtle RDF/webpagecounter_-_example.rdf > N3/webpagecounter_-_example.n3
 rapper -i rdfxml -o turtle RDF/playbackcounter_-_example.rdf > N3/playbackcounter_-_example.n3
--- examples/gen_xml.sh
+++ examples/gen_xml.sh
@@ -2,5 +2,5 @@
 
 # Generate the RDF/XML from the Turtle code
 
-rapper -i turtle -o rdfxml-abbrev N3/webcounter_-_example.n3 > RDF/webcounter_-_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/webpagecounter_-_example.n3 > RDF/webpagecounter_-_example.rdf
 rapper -i turtle -o rdfxml-abbrev N3/playbackcounter_-_example.n3 > RDF/playbackcounter_-_example.rdf
